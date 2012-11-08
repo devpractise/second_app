3b48adf46fd2225b6ca87fef5c42cae8f7f9cb8b
Robert Toledo <devpractise@gmail.com>
2012-11-07 20:37:57 -0800

    Add Test Driven Dev infrastructure + Guard + Spork + Sublime RubyTest

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+	let(:base_title) {"Ruby on Rails Tutorial Sample App"}
   describe "Home page" do
     it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -10,7 +11,7 @@ describe "StaticPages" do
     it "should have the right title 'Home'" do
     	visit '/static_pages/home'
     	page.should have_selector('title', 
-    		:text => "Ruby on Rails Tutorial Sample App | Home")
+    		:text => "#{base_title} | Home")
     end
   end
   describe "Help page" do
@@ -21,7 +22,7 @@ describe "StaticPages" do
   	it "should have the right title 'Help'" do
     	visit '/static_pages/help'
     	page.should have_selector('title', 
-    		:text => "Ruby on Rails Tutorial Sample App | Help")
+    		:text => "#{base_title} | Help")
     end
   end
   describe "About page" do
@@ -32,7 +33,10 @@ describe "StaticPages" do
   	it "should have the right title 'About Us'" do
     	visit '/static_pages/about'
     	page.should have_selector('title', 
-    		:text => "Ruby on Rails Tutorial Sample App | About Us")
+    		:text => "#{base_title} | About Us")
     end
+  end
+  it "should have a random page 'Testing'" do
+    visit '/static_pages/testing'
   end
 end
